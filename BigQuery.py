d0d3d61aab84c863dc298beea40802e596592812
learngcpwithmahesh <64518661+learngcpwithmahesh@users.noreply.github.com>
2020-10-17 17:25:13 +0800

    BigQuery file

---
+++ BigQuery.py
@@ -0,0 +1,27 @@
+#Step 0: Check whether pip is installed by typing pip --version or pip3 --version
+#Step 1: If it not installed type sudo apt install python3-pip -y
+#Step 2: Now, type pip3 install -r requirements.txt which install google-cloud-bigquery module
+#Step 3: python3 BigQuery.py 
+
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# Perform a query.
+query = """
+	SELECT name, SUM(number) as total_people
+	FROM `bigquery-public-data.usa_names.usa_1910_2013`
+	WHERE state = 'TX'
+	GROUP BY name, state
+	ORDER BY total_people ASC
+	LIMIT 10
+"""
+
+# Make an API request.
+query_job = client.query(query)  
+
+print("The query data:")
+for row in query_job:
+	# Row values can be accessed by field name or index.
+	print("name={}, count={}".format(row[0], row["total_people"]))	
